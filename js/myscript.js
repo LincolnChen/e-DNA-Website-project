c00d7a69a5ed04b4b5590f62d0ea5dbad7c83a89
Silin_Chen <silin.chen05@gmail.com>
2020-06-11 00:43:21 +0930

    Finalised topic pages

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,328 @@
-function show_building(x) {
-  document.getElementById("info__layer").style.display = "none";
-  document.getElementById("edna_info"+x).style.display = "block";
+//edna page//
 
+function show_building() {
+  document.getElementById("info__layer").style.display = "none";
+  document.getElementById("edna_info").style.display = "block";
 }
 
-function hide_building(x) {
+function hide_building() {
   document.getElementById("info__layer").style.display = "block";
-  document.getElementById("edna_info"+x).style.display = "none";
+  document.getElementById("edna_info").style.display = "none";
+}
+
+function show_edna() {
+  document.getElementById("edna__layer").style.display = "none";
+  document.getElementById("edna_info2").style.display = "block";
+}
+
+function hide_edna() {
+  document.getElementById("edna__layer").style.display = "block";
+  document.getElementById("edna_info2").style.display = "none";
+}
+
+
+function show_edna1() {
+  document.getElementById("edna2__layer").style.display = "none";
+  document.getElementById("edna_info3").style.display = "block";
+}
+
+function hide_edna2() {
+  document.getElementById("edna2__layer").style.display = "block";
+  document.getElementById("edna_info3").style.display = "none";
+}
+
+function show_edna2() {
+  document.getElementById("edna3__layer").style.display = "none";
+  document.getElementById("edna_info4").style.display = "block";
+}
+
+function hide_edna3() {
+  document.getElementById("edna3__layer").style.display = "block";
+  document.getElementById("edna_info4").style.display = "none";
+}
+
+function show_edna3() {
+  document.getElementById("edna4__layer").style.display = "none";
+  document.getElementById("edna_info5").style.display = "block";
+}
+
+function hide_edna4() {
+  document.getElementById("edna4__layer").style.display = "block";
+  document.getElementById("edna_info5").style.display = "none";
+}
+
+function show_edna4() {
+  document.getElementById("edna5__layer").style.display = "none";
+  document.getElementById("edna_info6").style.display = "block";
+}
+
+function hide_edna5() {
+  document.getElementById("edna5__layer").style.display = "block";
+  document.getElementById("edna_info6").style.display = "none";
+}
+
+function show_edna5() {
+  document.getElementById("edna6__layer").style.display = "none";
+  document.getElementById("edna_info7").style.display = "block";
+}
+
+function hide_edna6() {
+  document.getElementById("edna6__layer").style.display = "block";
+  document.getElementById("edna_info7").style.display = "none";
+}
+
+function show_edna7() {
+  document.getElementById("edna7__layer").style.display = "none";
+  document.getElementById("edna_info8").style.display = "block";
+}
+
+//end edna page//
+
+
+
+
+//gouldian starts//
+function show_gouldian() {
+  document.getElementById("gouldian__layer").style.display = "none";
+  document.getElementById("gouldian_info").style.display = "block";
+}
+
+function hide_gouldian() {
+  document.getElementById("gouldian__layer").style.display = "block";
+  document.getElementById("gouldian_info").style.display = "none";
+}
+
+function show_gouldian2() {
+  document.getElementById("gouldian2__layer").style.display = "none";
+  document.getElementById("gouldian_info2").style.display = "block";
+}
+
+function hide_gouldian2() {
+  document.getElementById("gouldian2__layer").style.display = "block";
+  document.getElementById("gouldian_info2").style.display = "none";
+}
+
+function show_gouldian3() {
+  document.getElementById("gouldian3__layer").style.display = "none";
+  document.getElementById("gouldian_info3").style.display = "block";
+}
+
+function hide_gouldian3() {
+  document.getElementById("gouldian3__layer").style.display = "block";
+  document.getElementById("gouldian_info3").style.display = "none";
+}
+//gouldian ends//
+
+
+//finch starts//
+function show_finch() {
+  document.getElementById("finch__layer").style.display = "none";
+  document.getElementById("finch_info").style.display = "block";
+}
+
+function hide_finch() {
+  document.getElementById("finch__layer").style.display = "block";
+  document.getElementById("finch_info").style.display = "none";
+}
+
+function show_finch2() {
+  document.getElementById("finch2__layer").style.display = "none";
+  document.getElementById("finch_info2").style.display = "block";
+}
+
+function hide_finch2() {
+  document.getElementById("finch2__layer").style.display = "block";
+  document.getElementById("finch_info2").style.display = "none";
+}
+
+function show_finch3() {
+  document.getElementById("finch3__layer").style.display = "none";
+  document.getElementById("finch_info3").style.display = "block";
+}
+
+function hide_finch3() {
+  document.getElementById("finch3__layer").style.display = "block";
+  document.getElementById("finch_info3").style.display = "none";
+}
+
+function show_finch4() {
+  document.getElementById("finch4__layer").style.display = "none";
+  document.getElementById("finch_info4").style.display = "block";
+}
+
+function hide_finch4() {
+  document.getElementById("finch4__layer").style.display = "block";
+  document.getElementById("finch_info4").style.display = "none";
+}
+
+function show_finch5() {
+  document.getElementById("finch5__layer").style.display = "none";
+  document.getElementById("finch_info5").style.display = "block";
+}
+
+function hide_finch5() {
+  document.getElementById("finch5__layer").style.display = "block";
+  document.getElementById("finch_info5").style.display = "none";
+}
+//finch ends//
+
+//conditions start//
+function show_condition() {
+  document.getElementById("condition__layer").style.display = "none";
+  document.getElementById("condition_info").style.display = "block";
+}
+
+function hide_condition() {
+  document.getElementById("condition__layer").style.display = "block";
+  document.getElementById("condition_info").style.display = "none";
+}
+
+function show_condition2() {
+  document.getElementById("condition2__layer").style.display = "none";
+  document.getElementById("condition_info2").style.display = "block";
+}
+
+function hide_condition2() {
+  document.getElementById("condition2__layer").style.display = "block";
+  document.getElementById("condition_info2").style.display = "none";
+}
+
+function show_condition3() {
+  document.getElementById("condition3__layer").style.display = "none";
+  document.getElementById("condition_info3").style.display = "block";
+}
+
+function hide_condition3() {
+  document.getElementById("condition3__layer").style.display = "block";
+  document.getElementById("condition_info3").style.display = "none";
+}
+
+function show_condition4() {
+  document.getElementById("condition4__layer").style.display = "none";
+  document.getElementById("condition_info4").style.display = "block";
+}
+
+function hide_condition4() {
+  document.getElementById("condition4__layer").style.display = "block";
+  document.getElementById("condition_info4").style.display = "none";
+}
+
+function show_condition5() {
+  document.getElementById("condition5__layer").style.display = "none";
+  document.getElementById("condition_info5").style.display = "block";
+}
+
+function hide_condition5() {
+  document.getElementById("condition5__layer").style.display = "block";
+  document.getElementById("condition_info5").style.display = "none";
+}
+
+function show_condition6() {
+  document.getElementById("condition6__layer").style.display = "none";
+  document.getElementById("condition_info6").style.display = "block";
+}
+
+function hide_condition6() {
+  document.getElementById("condition6__layer").style.display = "block";
+  document.getElementById("condition_info6").style.display = "none";
+}
+
+function show_condition7() {
+  document.getElementById("condition7__layer").style.display = "none";
+  document.getElementById("condition_info7").style.display = "block";
+}
+
+function hide_condition7() {
+  document.getElementById("condition7__layer").style.display = "block";
+  document.getElementById("condition_info7").style.display = "none";
+}
+
+function show_condition8() {
+  document.getElementById("condition8__layer").style.display = "none";
+  document.getElementById("condition_info8").style.display = "block";
+}
+
+function hide_condition8() {
+  document.getElementById("condition8__layer").style.display = "block";
+  document.getElementById("condition_info8").style.display = "none";
+}
+
+function show_condition9() {
+  document.getElementById("condition9__layer").style.display = "none";
+  document.getElementById("condition_info9").style.display = "block";
+}
+
+function hide_condition9() {
+  document.getElementById("condition9__layer").style.display = "block";
+  document.getElementById("condition_info9").style.display = "none";
+}
+
+function show_condition10() {
+  document.getElementById("condition10__layer").style.display = "none";
+  document.getElementById("condition_info10").style.display = "block";
+}
+
+function hide_condition10() {
+  document.getElementById("condition10__layer").style.display = "block";
+  document.getElementById("condition_info10").style.display = "none";
+}
+
+function show_condition11() {
+  document.getElementById("condition11__layer").style.display = "none";
+  document.getElementById("condition_info11").style.display = "block";
+}
+
+function hide_condition11() {
+  document.getElementById("condition11__layer").style.display = "block";
+  document.getElementById("condition_info11").style.display = "none";
+}
+
+function show_condition12() {
+  document.getElementById("condition12__layer").style.display = "none";
+  document.getElementById("condition_info12").style.display = "block";
+}
+
+function hide_condition12() {
+  document.getElementById("condition12__layer").style.display = "block";
+  document.getElementById("condition_info12").style.display = "none";
+}
+
+function show_condition13() {
+  document.getElementById("condition13__layer").style.display = "none";
+  document.getElementById("condition_info13").style.display = "block";
+}
+
+function hide_condition13() {
+  document.getElementById("condition13__layer").style.display = "block";
+  document.getElementById("condition_info13").style.display = "none";
+}
+
+function show_condition14() {
+  document.getElementById("condition14__layer").style.display = "none";
+  document.getElementById("condition_info14").style.display = "block";
+}
+
+function hide_condition14() {
+  document.getElementById("condition14__layer").style.display = "block";
+  document.getElementById("condition_info14").style.display = "none";
+}
+
+function show_condition15() {
+  document.getElementById("condition15__layer").style.display = "none";
+  document.getElementById("condition_info15").style.display = "block";
+}
+
+function hide_condition15() {
+  document.getElementById("condition15__layer").style.display = "block";
+  document.getElementById("condition_info15").style.display = "none";
+}
+
+function show_condition16() {
+  document.getElementById("condition16__layer").style.display = "none";
+  document.getElementById("condition_info16").style.display = "block";
+}
+
+function hide_condition16() {
+  document.getElementById("condition16__layer").style.display = "block";
+  document.getElementById("condition_info16").style.display = "none";
 }
