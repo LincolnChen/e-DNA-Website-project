57386ed8fa9dcdaea4b4f2a379b66e2032cb698a
Silin Chen <silin.chen05@gmail.com>
2020-11-13 01:12:36 +0930

    made a lot of changes

--- js/myscript.js
+++ js/myscript.js
@@ -1042,20 +1042,3 @@ function myFunction20() {
     x.style.display = "block";
   }
 }
-
-var d = document
-var w = window
-w.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (d.body.scrollTop > 20 || d.documentElement.scrollTop > 20) {
-    d.getElementById("backtotop").style.display = "block";
-  } else {
-    d.getElementById("backtotop").style.display = "none";
-  }
-}
-
-function topFunction() {
-  d.body.scrollTop = 0;
-  d.documentElement.scrollTop = 0;
-}
